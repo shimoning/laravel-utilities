cdeef7046529f57e31b231a5dc6b40827f75de1f
shimonhaga <shimon.haga@gmail.com>
2021-10-09 06:12:03 +0900

    Fix askWithValidation return type

--- src/Seeders/AskWithValidationTrait.php
+++ src/Seeders/AskWithValidationTrait.php
@@ -4,27 +4,36 @@ namespace Shimoning\LaravelUtilities\Seeders;
 
 trait AskWithValidationTrait
 {
+    /**
+     * コマンドライン入力をバリデートする
+     *
+     * @param string $question
+     * @param callable $validator
+     * @param string|int|null $default
+     * @return void
+     */
     public function askWithValidation(
         string $question,
         callable $validator,
         $default = null
-    ): string {
+    ): ?string {
         $input = $this->command->ask($question, $default);
         if (! $validator($input)) {
             $this->command->error('Input invalid. [ input = ' . ($input ?? 'null') . ' ]');
             return $this->askWithValidation($question, $validator, $default);
         }
 
-        return $input;
+        return $input ?? $default;
     }
 
     /**
      * Check input be null or numeric
-     *
-     * @param string $input
-     * @return boolean
-     */
-    public function nullableNumeric($input) {
+      *
+      * @param string|null $input
+      * @return bool
+      */
+    public function nullableNumeric(?string $input): bool
+    {
         return $input === null || \is_numeric($input);
     }
 }

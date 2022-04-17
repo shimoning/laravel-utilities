6b58c761cc8fc2100d1e36b22fa6633d05656841
shimonhaga <shimon.haga@gmail.com>
2022-04-17 10:12:34 +0900

    Fix default values config

--- config/laravel-utilities.php
+++ config/laravel-utilities.php
@@ -3,9 +3,9 @@
 return [
     // for Database logger
     'db_logging' => env('DB_LOGGING', false),
-    'db_logging_channel' => env('DB_LOGGING_CHANNEL', false),
+    'db_logging_channel' => env('DB_LOGGING_CHANNEL', null),
 
     // for Mail logger
     'mail_logging' => env('MAIL_LOGGING', false),
-    'mail_logging_channel' => env('MAIL_LOGGING_CHANNEL', false),
+    'mail_logging_channel' => env('MAIL_LOGGING_CHANNEL', null),
 ];
